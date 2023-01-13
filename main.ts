6df855b60f20c4c18082da7e63f62737746b3fd7
H4ZARD9 <122539729+H4ZARD9@users.noreply.github.com>
2023-01-13 08:59:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,15 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 B A B A B A B ", 500)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+})
+music.playMelody("C D E F G A B C5 ", 500)
 basic.showIcon(IconNames.Happy)
